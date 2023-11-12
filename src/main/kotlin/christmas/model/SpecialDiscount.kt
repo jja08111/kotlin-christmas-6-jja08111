072602ae0f8b3e366c98c3c3a19d0fc78472c16d
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-11-12 13:55:10 +0900

    feature(Discount): implement SpecialDoscount

---
+++ src/main/kotlin/christmas/model/SpecialDiscount.kt
@@ -0,0 +1,28 @@
+package christmas.model
+
+import java.time.LocalDate
+
+class SpecialDiscount(
+    private val date: LocalDate
+): Discount {
+    override val name: String = "특별 할인"
+
+    override fun calculate(): Int {
+        if (!DISCOUNT_DATES.contains(date)) {
+            return 0
+        }
+        return DISCOUNT_AMOUNT
+    }
+
+    companion object {
+        private const val DISCOUNT_AMOUNT = 1_000
+        private val DISCOUNT_DATES = listOf(
+            LocalDate.of(2023, 12, 3),
+            LocalDate.of(2023, 12, 10),
+            LocalDate.of(2023, 12, 17),
+            LocalDate.of(2023, 12, 24),
+            LocalDate.of(2023, 12, 25),
+            LocalDate.of(2023, 12, 31)
+        )
+    }
+}
---
+++ src/test/kotlin/christmas/model/SpecialDiscountTest.kt
@@ -0,0 +1,23 @@
+package christmas.model
+
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.time.LocalDate
+
+class SpecialDiscountTest {
+    @ParameterizedTest
+    @ValueSource(ints = [3, 10, 17, 24, 25, 31])
+    fun `특별 할인 일자에는 1,000원이 할인된다`(day: Int) {
+        val discount = SpecialDiscount(LocalDate.of(2023, 12, day))
+        val amount = discount.calculate()
+        assert(amount == 1_000)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 4, 16, 23, 26, 30])
+    fun `특별 할인 일자가 아닌 경우 할인은 없다`(day: Int) {
+        val discount = SpecialDiscount(LocalDate.of(2023, 12, day))
+        val amount = discount.calculate()
+        assert(amount == 0)
+    }
+}
